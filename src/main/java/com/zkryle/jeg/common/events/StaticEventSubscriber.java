67be75411fdcf18fdc5ccc6128ec8c83799d80ef
Matyrobbrt <matyrobbrt@gmail.com>
2021-12-31 19:37:43 +0200

    Make the plant golem simulate the placing of crops, as its (player) owner
    Signed-off-by: Matyrobbrt <matyrobbrt@gmail.com>

--- src/main/java/com/zkryle/jeg/common/events/StaticEventSubscriber.java
+++ src/main/java/com/zkryle/jeg/common/events/StaticEventSubscriber.java
@@ -1,11 +1,17 @@
 package com.zkryle.jeg.common.events;
 
+import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FACING;
+
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
 import com.zkryle.jeg.JustEnoughGolems;
 import com.zkryle.jeg.common.golem.PlantGolemEntity;
 import com.zkryle.jeg.core.Init;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.Direction;
@@ -14,12 +20,11 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FACING;
 
 @Mod.EventBusSubscriber(modid = JustEnoughGolems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class StaticEventSubscriber{
@@ -41,7 +46,15 @@ public class StaticEventSubscriber{
             pumpkin = pos.above();
             mossycobblestone = pos;
         } else return;
-        spawnPlantGolem(level, mossycobblestone, pumpkin);
+        
+        UUID ownerUUID = null;
+        // We only care about players, as we are simulating the player placing.
+        if (event.getEntity() instanceof PlayerEntity) {
+        	// A bit of an unnecesarry cast, but to be on the safe side
+        	ownerUUID = ((PlayerEntity) event.getEntity()).getUUID();
+        }
+        
+        spawnPlantGolem(level, mossycobblestone, pumpkin, ownerUUID);
     }
 
     @SubscribeEvent
@@ -53,17 +66,17 @@ public class StaticEventSubscriber{
             event.setCanceled(true);
             event.getWorld().playLocalSound(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), SoundEvents.PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0F, 1.0F, true);
             event.getPlayer().getMainHandItem().hurtAndBreak(1, event.getPlayer(), p -> p.broadcastBreakEvent( Hand.MAIN_HAND));
-            spawnPlantGolem(event.getWorld(), event.getPos().below(), event.getPos());
+            spawnPlantGolem(event.getWorld(), event.getPos().below(), event.getPos(), event.getPlayer().getUUID());
         }
     }
 
-    private static void spawnPlantGolem( World level, BlockPos mossycobblestone, BlockPos pumpkin){
+    private static void spawnPlantGolem( World level, BlockPos mossycobblestone, BlockPos pumpkin, @Nullable UUID owner){
         if(level.getBlockState( mossycobblestone ).getBlock() == Init.PRECIOUS_MOSSY_COBBLESTONE.get()
                 && level.getBlockState( pumpkin ).getBlock() == Blocks.CARVED_PUMPKIN){
             level.destroyBlock( pumpkin, false );
             level.destroyBlock( mossycobblestone, false );
-            PlantGolemEntity plantGolem = new PlantGolemEntity( Init.PLANT_GOLEM_ENTITY.get() , level );
-           plantGolem.setPos( mossycobblestone.getX() + 0.5D , mossycobblestone.getY() , mossycobblestone.getZ() + 0.5D );
+            PlantGolemEntity plantGolem = PlantGolemEntity.createGolem(owner, level);
+            plantGolem.setPos( mossycobblestone.getX() + 0.5D , mossycobblestone.getY() , mossycobblestone.getZ() + 0.5D );
             level.addFreshEntity( plantGolem );
         }
     }
